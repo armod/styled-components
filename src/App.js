a011a1f526290cbbdd9dadb02dd39f4fee0a51f7
armod <armods@gmail.com>
2022-11-03 20:04:40 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import styled from 'styled-components'
 import BasicTitle from './components/BasicTitle'
-import { DefaultButton } from './components/Buttons'
+import { DefaultButton, HipsterButton } from './components/Buttons'
+import { Button } from '@mui/material'
+
+const StyledBtn = styled(Button)`
+  text-transform: capitalize;
+`
 
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
-      <BasicTitle special>styled component</BasicTitle>
+      <Button color='primary' variant='contained'>
+        hello word
+      </Button>
+      <StyledBtn color='primary' variant='contained'>
+        hello word
+      </StyledBtn>
+      {/* <BasicTitle special>styled component</BasicTitle>
       <BasicTitle>styled component</BasicTitle>
       <DefaultButton>click me</DefaultButton>
+      <HipsterButton>click me</HipsterButton> */}
     </div>
   )
 }
